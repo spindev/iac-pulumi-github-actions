cb97a5c31df4c3aa124befb2dad5f5bdfa003004
Till Spindler <tillspi@web.de>
2023-03-01 19:45:26 +0100

    Initial commit

---
+++ 01-storage-account/Program.cs
@@ -0,0 +1,15 @@
+﻿using Pulumi;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
+
+return await Deployment.RunAsync(() =>
+{
+    var resourceGroup = new ResourceGroup("resourceGroup");
+
+    var storageAccount = new Account("storage", new AccountArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        AccountReplicationType = "LRS",
+        AccountTier = "Standard"
+    });
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# About
+Here will you find the examples and slides for my Talk about "IaC with Pulumi and GitHub Actions"
